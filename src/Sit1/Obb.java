4cbc71738de3fd24dc81c8f96fdc0e0c1df657b1
Sitkiku Love <Sitkiku Love>
2017-04-10 09:52:56 +0700

    ss

---
+++ src/Sit1/Obb.java
@@ -0,0 +1,15 @@
+package Sit1;
+
+public class Obb {
+
+	public static void main(String[] args) {
+		int i;
+		for(int i = 1;i <= 20 ;i++);
+		{
+			if(i % 2!=0);
+			System.out.println(i);
+		}
+
+	}
+
+}
